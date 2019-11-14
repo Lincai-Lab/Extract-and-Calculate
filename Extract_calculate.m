65d98a9afb81fbd99dc4f7c4cc8269523ae9c1ad
fkdcn <1091795514@qq.com>
2019-11-14 20:43:07 +0800

    Update Extract_calculate.m

--- Extract_calculate.m
+++ Extract_calculate.m
@@ -1,7 +1,7 @@
 A = csvread('filepath.e.g.E:\1\Data.csv');%拷贝文件路径
-FREQ = A(:,1);
-AMP = A(:,8);
-DEG = A(:,9);
+FREQ = A(:,1);%列号
+AMP = A(:,8);%列号
+DEG = A(:,9);%列号
 REAL = AMP.*sind(DEG);
 IMAG = AMP.*cosd(DEG);
 REAL = [FREQ, AMP];
